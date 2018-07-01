36dce7301f30685df12741e0a64895503315926f
edwingavis <egavis@uchicago.edu>
2018-07-01 18:31:25 -0500

    trying with cursor

--- collect.py
+++ collect.py
@@ -3,17 +3,43 @@
 ##Exploratory scraping of congressional tweets 
 
 import tweepy
+import csv
 
-auth = tweepy.OAuthHandler(get_codes(consumer = True))
-auth.set_access_token(get_codes(consumer = False))
+TARGET = ""
 
-api = tweepy.API(auth)
+def get_tweets(api, user):
+	'''
+	tweets = api.user_timeline(screen_name = user,count=200)
+	last = tweets[-1].id - 1
+	n_added = len(tweets)
+	while n_added:
+		tweets += api.user_timeline(screen_name = user,count=200, max_id = last)
+		n_added = len(tweets) - 
+	'''
+	s = tweepy.Cursor(api.user_timeline, screen_name = user, include_rts = True)
+	tweets = []
+	while True:
+		try:
+			t = c.next()
+			tweets.append([t.id_str, t.created_at, t.text.encode("utf-8")])
+		except tweepy.TweepError:
+       		time.sleep(60 * 15)
+        	continue
+		except StopIteration:
+			break 
+	write_tweets(tweets, user)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-	print tweet.text
+def write_tweets(t, user):
+	with open('%s_tweets.csv' % user, 'wb') as f:
+		csvwriter = csv.writer(f)
+		writer.writerow(["id","created_at","text"])
+		writer.writerows(t)
 	
-
+def tweep_init():
+	auth = tweepy.OAuthHandler(get_codes(consumer = True))
+	auth.set_access_token(get_codes(consumer = False))
+	api = tweepy.API(auth)
+	return api
 
 def get_codes(consumer):
 	target = "auth/access"
@@ -23,3 +49,6 @@ def get_codes(consumer):
 		codes = f.readlines()
 		codes = [c.strip() for c in codes]
 	return codes[0], codes[1]
+	
+if __name__ == '__main__':
+	pass
