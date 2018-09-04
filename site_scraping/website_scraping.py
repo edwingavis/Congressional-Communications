8c023b1115c9e0d084c5d018fbb39b2c59d4d689
edwingavis <egavis@uchicago.edu>
2018-09-04 15:27:07 -0500

    wip

--- site_scraping/website_scraping.py
+++ site_scraping/website_scraping.py
@@ -1,23 +1,68 @@
-import urllib3 
+import requests
 import time
-#import progressbar
+import progressbar
 import bs4
 import random
+import csv
 import os
 
 #USE BREHM->SEARCHES TO PULL TWITTER ACCOUNTS!!!
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-http = urllib3.PoolManager()    
+#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#http = urllib3.PoolManager()    
 
-def scrape(site, state, race, name):
-    checked = {}
+UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+#pretending to be chrome
+
+CAMPAIGNS = True
+HOUSE = False
+
+STOPPED = ("CA", "28")
+#issues:
+#tabitha isner -- AL2 -- some kind of SPA
+#PROBABLY MISSED A FEW OTHERS IN FIRST 42, 
+#could just filter for len(os.listdir)==1 
+#greg stanton, not sure why campaign not work: stantonforarizona.com
+#jared huffman CA2 - campaign site
+#DALE MENSING CA2 - campaign site
+#salud carbajal - error every page
+#mark reed ca30
+
+def main():
+    with open("../lists/house_2018_sites_gov.csv") as csvfile:
+        spamreader = csv.reader(csvfile)
+        for _ in range(0): 
+            next(spamreader)
+            #skip don young... mike rogers
+        bar = progressbar.ProgressBar()
+        cont = True
+        for row in bar(spamreader):
+            state, race= row[0:2]
+            name, c_site, g_site = row[3:6]
+            #house site:
+            if (state, race) == STOPPED:
+                cont = False
+                #continue #if second candidate
+            if cont:
+                continue
+            print("\nScraping %s %s %s" % (state, race, name))
+            if HOUSE and g_site:
+                house_folder = "../data/%s/%s/%s/house_site" % (state, race, name)
+                os.makedirs(house_folder, exist_ok = True)
+                scrape(g_site, house_folder)
+            if CAMPAIGNS and c_site:
+                camp_folder = "../data/%s/%s/%s/elect_site" % (state, race, name)
+                os.makedirs(camp_folder, exist_ok = True)
+                scrape(c_site, camp_folder)  
+
+def scrape(site, folder):#, state, race, name):
+    checked = set()
     remaining = [site] #may use another data structure (collections.)
     while remaining:
         url = remaining.pop() #LIFO
-        if url in checked:
+        if url.lower().strip() in checked or check_url(url):
             continue
-        checked.add(url)
+        checked.add(url.lower().strip())
         html = get_html(url)
         if not html:
             continue #may not be necessary 
@@ -25,16 +70,31 @@ def scrape(site, state, race, name):
         for link in links: 
             remaining.append(link)
         #write str(soup) to file
-        write_file(soup)
+        write_file(folder, url, soup)#, state, race, name)
+        sleep_time = 3 * random.random()
+        time.sleep(sleep_time)
       
-def write_file(site, state, race, soup):
+def check_url(url):
+    known = {"watch", "youtube", ".aspx", "#", ".pdf", 
+             "user", "twitter", "facebook", "mailto",
+             ".jpg", ".png", ".bmp", ".jpeg", ".xls", "tel:",
+             "ical", "javascript", "action", "gallery", ".mp4"}
+    lowered = url.lower()
+    for v in known:
+        if v in lowered:
+            return True
+    return False
+        
+def write_file(folder, url, soup):
     '''
     '''
-    file_path = 'propublica/' + state + '_' + str(race)
-    with open(file_path, 'w') as f: 
-        #print("Writing FILE %s" % file_path)
+    fname = "/" + url.strip("https: ").replace(
+                       "//","").replace("/","_").replace(".","_")
+    if len(fname) > 150:
+        fname = fname[:150]
+    print("Scraped:\t%s" % fname)
+    with open(folder + fname + '.html', 'w') as f: 
         f.write(str(soup))
-
         
 def get_html(url):
     '''
@@ -42,8 +102,8 @@ def get_html(url):
     #put try/except in here 
     rv = "" #may be wrong datatype
     try:
-        r = http.request('GET', url)
-        rv = r.data
+        header = {'User-Agent':UA}
+        rv = requests.get(url, headers=header).text
     except:
         print("Error scraping %s" % url)
     return rv 
@@ -53,32 +113,22 @@ def get_soup_links(html, site):
     '''
     #finish
     soup = bs4.BeautifulSoup(html, 'lxml')
-    new_links = soup.find_all("a")
-    links = {} #need to make it only links to same site (+relative)
+    new_links = soup.find_all("a", href=True)
+    links = set() #need to make it only links to same site (+relative)
+    #print(new_links)
     for l in new_links:
-        if site in l: 
+        if site in l['href']: 
         #p needs something like l.data / l.html etc. 
         #plus account for relative links
         #could speedup w/ regex search?
-            links.add(l)
+            links.add(l['href'])
+        elif "/" not in l['href'] or l['href'][0] == "/":
+            relative = l['href'].strip("/")
+            combined = site + relative
+            links.add(combined)
     return soup, links
 
-###############################
-     
-def main():
-    '''
-    deprecated, kept for time.sleep(random) examples
-    '''
-    #i = 0
-    bar = progressbar.ProgressBar()
-    for state in bar(elections.states[5:]):
-        for race in range(elections.races[state]):
-            url = build_url(state, race + 1) 
-            html = get_html(url)
-            write_file(html, state, race)
-            sleep_time = random.randint(4,7)
-            time.sleep(sleep_time)
-        time.sleep(sleep_time * max(random.random() * 2, 1))
-    
+#time.sleep(sleep_time * max(random.random() * 2, 1))
+
 if __name__ == '__main__':
-	pass
+	main()
